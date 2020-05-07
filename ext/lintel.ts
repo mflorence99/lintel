1ee1357393f1b44cbf44164746a24151cbad81b0
Mark Florence <mflo999@gmail.com>
2020-05-07 10:19:31 -0400

    Simulate VSCode env in Angular app

--- ext/lintel.ts
+++ ext/lintel.ts
@@ -2,6 +2,8 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as vscode from 'vscode';
 
+/** @see https://code.visualstudio.com/api/extension-guides/webview */
+
 export function activate(context: vscode.ExtensionContext): void {
 
   let currentPanel: vscode.WebviewPanel | undefined = undefined;
@@ -9,22 +11,27 @@ export function activate(context: vscode.ExtensionContext): void {
   context.subscriptions.push(
     vscode.commands.registerCommand('lintel.start', () => {
 
+      // what column to use?
       const columnToShowIn = vscode.window.activeTextEditor
         ? vscode.window.activeTextEditor.viewColumn
         : undefined;    
 
+      // only show one instance at a time
       if (currentPanel) 
         currentPanel.reveal(columnToShowIn);        
       else currentPanel = vscode.window.createWebviewPanel('lintel', 'Lintel', columnToShowIn as any, { enableScripts: true });
 
+      // clean up when we're done
+      currentPanel.onDidDispose(() => currentPanel = undefined, null, context.subscriptions);
+
+      // munge the Angular app's index.html
       const appDistPath = path.join(context.extensionPath, 'dist/lintel');
       const appDistPathUri = vscode.Uri.file(appDistPath);
       const baseUri = currentPanel.webview.asWebviewUri(appDistPathUri);
       const indexPath = path.join(appDistPath, 'index.html');
-      const indexHtml = fs.readFileSync(indexPath, { encoding: 'utf8' });
-      currentPanel.webview.html = indexHtml.replace('<base href="/">', `<base href="${String(baseUri)}/">`);
-
-      currentPanel.onDidDispose(() => currentPanel = undefined, null, context.subscriptions);
+      const indexHtml = fs.readFileSync(indexPath, { encoding: 'utf8' })
+        .replace('<base href="/">', `<base href="${String(baseUri)}/">`);
+      currentPanel.webview.html = indexHtml;
 
     })
 
---
+++ src/assets/vscode-scripts.js
@@ -0,0 +1,3 @@
+const acquireVsCodeApi = (function () {
+  // TBD
+})();
