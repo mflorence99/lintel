928098d577d0f95773df09193aa17577679875b8
Mark Florence <mflo999@gmail.com>
2020-06-17 07:25:33 -0400

    Close to 100% coverage on input-array.ts

--- src/app/components/input-array.spec.ts
+++ src/app/components/input-array.spec.ts
@@ -1,3 +1,4 @@
+import { FormArray } from '@angular/forms';
 import { InputArrayComponent } from './input-array';
 import { TestBed } from '@angular/core/testing';
 
@@ -12,6 +13,72 @@ describe('InputArrayComponent', () => {
     const fixture = TestBed.createComponent(InputArrayComponent);
     const component = fixture.componentInstance;
     expect(component).toBeTruthy();
+  });
+
+  test('registerOnChange', () => {
+    const fixture = TestBed.createComponent(InputArrayComponent);
+    const component = fixture.componentInstance;
+    component.registerOnChange(jest.fn());
+    expect(component['onChange']).toBeTruthy();
+  });
+
+  test('registerOnTouched', () => {
+    const fixture = TestBed.createComponent(InputArrayComponent);
+    const component = fixture.componentInstance;
+    expect(component.registerOnTouched(null)).toBeFalsy();
+  });
+
+  test('writeValue of number', () => {
+    const fixture = TestBed.createComponent(InputArrayComponent);
+    const component = fixture.componentInstance;
+    component.type = 'number';
+    expect(component.value).toEqual([]);
+    component.writeValue([1, 2, 3]);
+    expect(component.value).toEqual([1, 2, 3]);
+  });
+
+  test('writeValue of string', () => {
+    const fixture = TestBed.createComponent(InputArrayComponent);
+    const component = fixture.componentInstance;
+    component.type = 'text';
+    expect(component.value).toEqual([]);
+    component.writeValue(['London', 'Paris', 'Rome']);
+    expect(component.value).toEqual(['London', 'Paris', 'Rome']);
+  });
+
+  test('addInput', () => {
+    const fixture = TestBed.createComponent(InputArrayComponent);
+    const component = fixture.componentInstance;
+    component.type = 'text';
+    component.value = ['London', 'Paris', 'Rome'];
+    expect((component.inputArrayForm.controls.inputs as FormArray).length).toEqual(4);
+    component.addInput();
+    expect((component.inputArrayForm.controls.inputs as FormArray).length).toEqual(5);
+  });
+
+  test('removeInput', () => {
+    const fixture = TestBed.createComponent(InputArrayComponent);
+    const component = fixture.componentInstance;
+    component.type = 'number';
+    component.value = [1, 2, 3];
+    expect((component.inputArrayForm.controls.inputs as FormArray).length).toEqual(4);
+    component.removeInput(1);
+    expect((component.inputArrayForm.controls.inputs as FormArray).length).toEqual(3);
+  });
+
+  test('ngOnInit', done => {
+    const fixture = TestBed.createComponent(InputArrayComponent);
+    const component = fixture.componentInstance;
+    component.type = 'text';
+    component.uniqueItems = true;
+    component.value = ['London', 'Paris', 'Paris', 'Rome'];
+    component.registerOnChange(value => {
+      expect(value).toEqual(['London', 'Paris', 'Rome']);
+      done();
+    });
+    component.ngOnInit();
+    // NOTE: trips valueChanges
+    component.addInput();
   });
 
 });
--- src/app/components/input.ts
+++ src/app/components/input.ts
@@ -56,7 +56,7 @@ export class InputComponent implements ControlValueAccessor {
     this._value = value;
     if (this.input?.nativeElement)
       this.input.nativeElement.value = value;
-    this.onChange?.(value);
+    this.onChange?.((this.type === 'number') ? Number(value) : value);
     // TODO: Angular can be so weird!
     this.cdf.detectChanges();
   }
