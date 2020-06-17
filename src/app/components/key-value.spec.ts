060266748b594be66ea143646d4205bb15945f05
Mark Florence <mflo999@gmail.com>
2020-06-17 15:45:43 -0400

    Good coverage on key-value.ts

--- src/app/components/key-value.spec.ts
+++ src/app/components/key-value.spec.ts
@@ -1,3 +1,4 @@
+import { FormArray } from '@angular/forms';
 import { KeyValueComponent } from './key-value';
 import { TestBed } from '@angular/core/testing';
 
@@ -12,6 +13,74 @@ describe('KeyValueComponent', () => {
     const fixture = TestBed.createComponent(KeyValueComponent);
     const component = fixture.componentInstance;
     expect(component).toBeTruthy();
+  });
+
+  test('registerOnChange', () => {
+    const fixture = TestBed.createComponent(KeyValueComponent);
+    const component = fixture.componentInstance;
+    component.registerOnChange(jest.fn());
+    expect(component['onChange']).toBeTruthy();
+  });
+
+  test('registerOnTouched', () => {
+    const fixture = TestBed.createComponent(KeyValueComponent);
+    const component = fixture.componentInstance;
+    expect(component.registerOnTouched(null)).toBeFalsy();
+  });
+
+  test('writeValue', () => {
+    const fixture = TestBed.createComponent(KeyValueComponent);
+    const component = fixture.componentInstance;
+    expect(component.value).toEqual({ });
+    const value = { a: 1, b: 2, c: '3' };
+    component.writeValue(value);
+    expect(component.value).toEqual(value);
+  });
+
+  test('addKeyValue', () => {
+    const fixture = TestBed.createComponent(KeyValueComponent);
+    const component = fixture.componentInstance;
+    component.value = { a: 1, b: 2, c: '3' };
+    expect((component.keyValueForm.controls.keyValues as FormArray).length).toEqual(4);
+    component.addKeyValue();
+    expect((component.keyValueForm.controls.keyValues as FormArray).length).toEqual(5);
+  });
+
+  test('removeKeyValue', () => {
+    const fixture = TestBed.createComponent(KeyValueComponent);
+    const component = fixture.componentInstance;
+    component.value = { a: 1, b: 2, c: '3' };
+    expect((component.keyValueForm.controls.keyValues as FormArray).length).toEqual(4);
+    component.removeKeyValue(1);
+    expect((component.keyValueForm.controls.keyValues as FormArray).length).toEqual(3);
+  });
+
+  test('ngOnInit for numbers', done => {
+    const fixture = TestBed.createComponent(KeyValueComponent);
+    const component = fixture.componentInstance;
+    component.type = 'number';
+    component.value = { a: 1, b: 2, c: '3' };
+    component.registerOnChange(value => {
+      expect(value).toEqual({ a: 1, b: 2, c: 3 });
+      done();
+    });
+    component.ngOnInit();
+    // NOTE: trips valueChanges
+    component.keyValueForm.updateValueAndValidity({ emitEvent: true });
+  });
+
+  test('ngOnInit for text', done => {
+    const fixture = TestBed.createComponent(KeyValueComponent);
+    const component = fixture.componentInstance;
+    component.type = 'text';
+    component.value = { a: 'true', b: 'false', c: '3' };
+    component.registerOnChange(value => {
+      expect(value).toEqual({ a: true, b: false, c: 3 });
+      done();
+    });
+    component.ngOnInit();
+    // NOTE: trips valueChanges
+    component.keyValueForm.updateValueAndValidity({ emitEvent: true });
   });
 
 });
