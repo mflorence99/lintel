8c51b4a0c1e1bcb65bba26e9c1441fee0d1a8168
Mark Florence <mflo999@gmail.com>
2020-06-26 05:15:49 -0400

    Test cases for rules were forgotten

--- src/app/pages/components/rules.spec.ts
+++ src/app/pages/components/rules.spec.ts
@@ -1,8 +1,11 @@
+import { RuleDigest } from '../../state/configs';
 import { RulesComponent } from './rules';
 import { TestBed } from '@angular/core/testing';
 
 import { async } from '@angular/core/testing';
 import { prepare } from './component.spec';
+
+declare const lintelVSCodeAPI;
 
 describe('RulesComponent', () => {
 
@@ -12,6 +15,49 @@ describe('RulesComponent', () => {
     const fixture = TestBed.createComponent(RulesComponent);
     const component = fixture.componentInstance;
     expect(component).toBeTruthy();
+  });
+
+  test('canExportRule - in config', () => {
+    const fixture = TestBed.createComponent(RulesComponent);
+    const component = fixture.componentInstance;
+    component.selection.select({ fileName: '/home/mflorence99/lintel/package.json', pluginName: '@typescript-eslint' });
+    expect(component.canExportRule({ ruleName: '@typescript-eslint/member-ordering' } as RuleDigest)).toBe(true);
+  });
+
+  test('canExportRule - in extension', () => {
+    const fixture = TestBed.createComponent(RulesComponent);
+    const component = fixture.componentInstance;
+    component.selection.select({ fileName: '/home/mflorence99/lintel/package.json', pluginName: 'vue' });
+    expect(component.canExportRule({ ruleName: 'vue/attributes-order' } as RuleDigest)).toBe(true);
+  });
+
+  test('execute deletes rule', () => {
+    const fixture = TestBed.createComponent(RulesComponent);
+    const component = fixture.componentInstance;
+    component.selection.select({ fileName: '/home/mflorence99/lintel/package.json', pluginName: '@typescript-eslint' });
+    const ruleName = '@typescript-eslint/member-ordering';
+    expect(component.configs.configuration.rules[ruleName]).toBeTruthy();
+    component.execute({ ruleName } as RuleDigest, 'delete');
+    expect(component.configs.configuration.rules[ruleName]).toBeFalsy();
+  });
+
+  test('execute exports rule', () => {
+    const fixture = TestBed.createComponent(RulesComponent);
+    const component = fixture.componentInstance;
+    component.selection.select({ fileName: '/home/mflorence99/lintel/package.json', pluginName: '@typescript-eslint' });
+    const ruleName = '@typescript-eslint/member-ordering';
+    component.execute({ ruleName } as RuleDigest, 'export');
+    const calls = lintelVSCodeAPI.postMessage.mock.calls;
+    expect(calls.length).toBeGreaterThanOrEqual(1);
+    const message = calls[calls.length - 1][0];
+    expect(message.command).toEqual('clipboardCopy');
+  });
+
+  test('isRuleDefined', () => {
+    const fixture = TestBed.createComponent(RulesComponent);
+    const component = fixture.componentInstance;
+    expect(component.isRuleDefined({ defined: true } as RuleDigest)).toBe(true);
+    expect(component.isRuleDefined({ defined: false } as RuleDigest)).toBe(false);
   });
 
   test('trackByRule', () => {
