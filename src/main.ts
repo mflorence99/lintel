94e3e8b30c4a558e41f6858077036a1e730151d6
Mark Florence <mflo999@gmail.com>
2020-10-17 06:55:18 -0400

    Update deps

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { LintelModule } from './app/module';
 
 import { environment } from './environments/environment';
 
+import { ApplicationRef } from '@angular/core';
+
+import { enableDebugTools } from '@angular/platform-browser';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -15,6 +18,14 @@ lintelIsReady.then(() => {
   if (window['eslintFiles'])
     platformBrowserDynamic()
       .bootstrapModule(LintelModule)
+      .then((moduleRef) => {
+        // @see https://blog.ninja-squad.com/2018/09/20/angular-performances-part-3/
+        if (!environment.production) {
+          const applicationRef = moduleRef.injector.get(ApplicationRef);
+          const componentRef = applicationRef.components[0];
+          enableDebugTools(componentRef);
+        }
+      })
       .catch((err) => console.error(err));
   else
     lintelVSCodeAPI.postMessage({
